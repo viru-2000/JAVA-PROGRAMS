faa4b7ad1f9f9f721b0c8378e15c60329ce90da5
viraj anil rasal <91334791+viru-2000@users.noreply.github.com>
2022-12-13 23:15:07 +0530

    Add files via upload

---
+++ Display Variables.java
@@ -0,0 +1,40 @@
+//Display Variables  println()
+
+
+
+1)
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+}
+
+
+
+//+ variable add character 
+2)
+
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+}
+
+
+
+
+3)
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    System.out.println(x + y); // Print the value of x + y
+  }
+}
